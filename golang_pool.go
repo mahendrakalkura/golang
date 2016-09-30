dc91f219808f8953ec01f6808c27e4755bc26a0c
Mahendra Kalkura <mahendra@kalkura.com>
2016-09-30 10:54:33 +0530

    Pool

--- golang_pool.go
+++ golang_pool.go
@@ -4,54 +4,52 @@ import "fmt"
 import "os"
 import "strconv"
 import "sync"
-import "time"
 
 var wg sync.WaitGroup
 
 func producer(replies <-chan int, iterations int) {
-    defer wg.Done()
-    total := 0
-    for index := 1; index <= iterations; index++ {
-        reply := <-replies
-        total = total + reply
-    }
-    fmt.Println(total)
+	defer wg.Done()
+	total := 0
+	for index := 1; index <= iterations; index++ {
+		reply := <-replies
+		total = total + reply
+	}
+	fmt.Println(total)
 }
 
 func consumer(tasks <-chan int, replies chan<- int) {
-    defer wg.Done()
-    for task := range tasks {
-        time.Sleep(time.Millisecond * 100)
-        replies <- task
-    }
+	defer wg.Done()
+	for task := range tasks {
+		replies <- task
+	}
 }
 
 func main() {
-    arguments := os.Args[1:]
+	arguments := os.Args[1:]
 
-    consumers, err := strconv.Atoi(arguments[0])
-    if err != nil {
-        panic(err)
-    }
+	consumers, err := strconv.Atoi(arguments[0])
+	if err != nil {
+		panic(err)
+	}
 
-    iterations, err := strconv.Atoi(arguments[1])
-    if err != nil {
-        panic(err)
-    }
+	iterations, err := strconv.Atoi(arguments[1])
+	if err != nil {
+		panic(err)
+	}
 
-    tasks := make(chan int, iterations)
-    replies := make(chan int, iterations)
+	tasks := make(chan int, iterations)
+	replies := make(chan int, iterations)
 
-    wg.Add(1)
-    go producer(replies, iterations)
+	wg.Add(1)
+	go producer(replies, iterations)
 
-    for id := 1; id <= consumers; id++ {
-        wg.Add(1)
-        go consumer(tasks, replies)
-    }
+	for id := 1; id <= consumers; id++ {
+		wg.Add(1)
+		go consumer(tasks, replies)
+	}
 
-    for index := 1; index <= iterations; index++ {
-        tasks <- index
+	for index := 1; index <= iterations; index++ {
+		tasks <- index
 	}
 	close(tasks)
 
